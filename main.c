0570f060fb340c02f028ca2f2131fbf92631df24
HalfdanElvis <halfdanelvis@gmail.com>
2025-09-10 14:19:19 +0200

    Grayscale added

--- main.c
+++ main.c
@@ -26,6 +26,36 @@ void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsi
   unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
   unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 
+void grayscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      
+      int gray = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
+
+      for (int c = 0; c < BMP_CHANNELS; c++){
+        output_image[x][y][c] = gray;
+      }
+    }
+  }
+}
+
+void grayscale2(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      
+      int gray = (input_image[x][y][0] * 0.299 + input_image[x][y][1] *0.587 + input_image[x][y][2] * 0.114);
+
+      for (int c = 0; c < BMP_CHANNELS; c++){
+        output_image[x][y][c] = gray;
+      }
+    }
+  }
+}
+
 //Main function
 int main(int argc, char** argv)
 {
@@ -47,7 +77,7 @@ int main(int argc, char** argv)
   read_bitmap(argv[1], input_image);
 
   //Run inversion
-  invert(input_image,output_image);
+  grayscale(input_image,output_image);
 
   //Save image to file
   write_bitmap(output_image, argv[2]);
