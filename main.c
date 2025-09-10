b91db02a2fc67d9c54eed27519c57aa9a9df14d4
joha7809 <joha7809@gmail.com>
2025-09-10 16:30:55 +0200

    added pointer swapping

--- main.c
+++ main.c
@@ -1,115 +1,118 @@
-//To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
-//To run (linux/mac): ./main.out example.bmp example_inv.bmp
+// To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
+// To run (linux/mac): ./main.out example.bmp example_inv.bmp
 
-//To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-//To run (win): main.exe example.bmp example_inv.bmp
+// To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
+// To run (win): main.exe example.bmp example_inv.bmp
 
-#include <stdlib.h>
-#include <stdio.h>
 #include "cbmp.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char temp_image[BMP_WIDTH][BMP_HEIGTH];
-  unsigned char temp_image2[BMP_WIDTH][BMP_HEIGTH];
+// Declaring the array to store the image (unsigned char = unsigned 8 bit)
+unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char temp_image[BMP_WIDTH][BMP_HEIGTH];
+unsigned char temp_image2[BMP_WIDTH][BMP_HEIGTH];
 
-  
-void grayscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]){
+void grayscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS],
+               unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]) {
   for (int x = 0; x < BMP_WIDTH; x++) {
     for (int y = 0; y < BMP_HEIGTH; y++) {
-      int gray = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
-      
+      int gray =
+          (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) /
+          3;
+
       output_image[x][y] = gray;
     }
   }
 }
 
-void grayscale2(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-      
-      int gray = (input_image[x][y][0] * 0.299 + input_image[x][y][1] *0.587 + input_image[x][y][2] * 0.114);
+void grayscale2(
+    unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS],
+    unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
+  for (int x = 0; x < BMP_WIDTH; x++) {
+    for (int y = 0; y < BMP_HEIGTH; y++) {
 
-      for (int c = 0; c < BMP_CHANNELS; c++){
+      int gray = (input_image[x][y][0] * 0.299 + input_image[x][y][1] * 0.587 +
+                  input_image[x][y][2] * 0.114);
+
+      for (int c = 0; c < BMP_CHANNELS; c++) {
         output_image[x][y][c] = gray;
       }
     }
   }
 }
 
-void threshold(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]){
+void threshold(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH],
+               unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]) {
   for (int x = 0; x < BMP_WIDTH; x++) {
-    for (int y = 0; y < BMP_HEIGTH; y++) {      
+    for (int y = 0; y < BMP_HEIGTH; y++) {
       if (input_image[x][y] <= 90) {
         output_image[x][y] = 0;
       } else {
         output_image[x][y] = 255;
-      }   
+      }
     }
   }
 }
 
-int erode(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]){
-  
+int erode(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH],
+          unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]) {
+
   int change = 0;
   // Se om dette kan fikses
-  for (int x = 1; x < BMP_WIDTH-1; x++) {
-    for (int y = 1; y < BMP_HEIGTH-1; y++) { 
+  for (int x = 1; x < BMP_WIDTH - 1; x++) {
+    for (int y = 1; y < BMP_HEIGTH - 1; y++) {
 
       if (input_image[x][y] == 255) {
-  
-        if (input_image[x-1][y] == 0  || input_image[x][y-1] == 0 || input_image[x][y+1] == 0 || input_image[x+1][y] == 0) {
+
+        if (input_image[x - 1][y] == 0 || input_image[x][y - 1] == 0 ||
+            input_image[x][y + 1] == 0 || input_image[x + 1][y] == 0) {
           output_image[x][y] = 0;
           change = 1;
         } else {
           output_image[x][y] = 255;
         }
+      } else {
+        output_image[x][y] = 0;
       }
     }
   }
   return change;
 }
 
+// Main function
+int main(int argc, char **argv) {
+  // argc counts how may arguments are passed
+  // argv[0] is a string with the name of the program
+  // argv[1] is the first command line argument (input image)
+  // argv[2] is the second command line argument (output image)
 
-//Main function
-int main(int argc, char** argv)
-{
-  //argc counts how may arguments are passed
-  //argv[0] is a string with the name of the program
-  //argv[1] is the first command line argument (input image)
-  //argv[2] is the second command line argument (output image)
-
-  //Checking that 2 arguments are passed
-  if (argc != 3)
-  {
-      fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-      exit(1);
+  // Checking that 2 arguments are passed
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <output file path> <output file path>\n",
+            argv[0]);
+    exit(1);
   }
 
   printf("Example program - 02132 - A1\n");
 
-  //Load image from file
+  // Load image from file
   read_bitmap(argv[1], input_image);
 
-  //Run inversion
+  // Run inversion
   grayscale(input_image, temp_image);
 
   threshold(temp_image, temp_image);
 
   erode(temp_image, temp_image2);
- 
-  //Save image to file
 
+  // Save image to file
 
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-      
-      for (int c = 0; c < BMP_CHANNELS; c++){
+  for (int x = 0; x < BMP_WIDTH; x++) {
+    for (int y = 0; y < BMP_HEIGTH; y++) {
+
+      for (int c = 0; c < BMP_CHANNELS; c++) {
         output_image[x][y][c] = temp_image[x][y];
       }
     }
@@ -117,51 +120,43 @@ int main(int argc, char** argv)
 
   write_bitmap(output_image, argv[2]);
 
+  for (int x = 0; x < BMP_WIDTH; x++) {
+    for (int y = 0; y < BMP_HEIGTH; y++) {
 
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-      
-      for (int c = 0; c < BMP_CHANNELS; c++){
+      for (int c = 0; c < BMP_CHANNELS; c++) {
         output_image[x][y][c] = temp_image2[x][y];
       }
     }
   }
-  
-  write_bitmap(output_image, argv[2]+1);
 
-  int temp = 10;
-  while(temp) {
-    erode(temp_image2, temp_image);
-    for (int x = 0; x < BMP_WIDTH; x++)
-    {
-      for (int y = 0; y < BMP_HEIGTH; y++)
-      {
-        
-        for (int c = 0; c < BMP_CHANNELS; c++){
-          temp_image2[x][y] = temp_image[x][y];
-        }
-      }
+  write_bitmap(output_image, argv[2] + 1);
+  unsigned char (*input)[BMP_HEIGTH] = temp_image;
+  unsigned char (*output)[BMP_HEIGTH] = temp_image2;
+
+  int change = 1;
+  int t = 0;
+  while (change) {
+    if (t == 10) {
+      break;
     }
+    t++;
+    change = erode(input, output);
 
-    temp--;
+    unsigned char (*tmp)[BMP_HEIGTH] = input;
+    input = output;
+    output = tmp;
   }
 
+  for (int x = 0; x < BMP_WIDTH; x++) {
+    for (int y = 0; y < BMP_HEIGTH; y++) {
 
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-      
-      for (int c = 0; c < BMP_CHANNELS; c++){
-        output_image[x][y][c] = temp_image2[x][y];
+      for (int c = 0; c < BMP_CHANNELS; c++) {
+        output_image[x][y][c] = output[x][y];
       }
     }
   }
-  
-  write_bitmap(output_image, argv[2]+2);
 
+  write_bitmap(output_image, argv[2] + 2);
 
   printf("Done!\n");
   return 0;
