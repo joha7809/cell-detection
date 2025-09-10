58c32987456bd3ab61465cb9a0a5abbd6fbd56ad
HalfdanElvis <halfdanelvis@gmail.com>
2025-09-10 15:42:49 +0200

    øh

--- main.c
+++ main.c
@@ -8,36 +8,19 @@
 #include <stdio.h>
 #include "cbmp.h"
 
-//Function to invert pixels of an image (negative)
-void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-      for (int c = 0; c < BMP_CHANNELS; c++)
-      {
-      output_image[x][y][c] = 255 - input_image[x][y][c];
-      }
-    }
-  }
-}
-
   //Declaring the array to store the image (unsigned char = unsigned 8 bit)
   unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
   unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+  unsigned char temp_image[BMP_WIDTH][BMP_HEIGTH];
+  unsigned char temp_image2[BMP_WIDTH][BMP_HEIGTH];
 
-void grayscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-      
+  
+void grayscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]){
+  for (int x = 0; x < BMP_WIDTH; x++) {
+    for (int y = 0; y < BMP_HEIGTH; y++) {
       int gray = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
-      
       // se på om man kan optimere c ved at gøre det fra array til en integer
-      for (int c = 0; c < BMP_CHANNELS; c++){
-        output_image[x][y][c] = gray;
-      }
+      output_image[x][y] = gray;
     }
   }
 }
@@ -57,31 +40,39 @@ void grayscale2(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], 
   }
 }
 
-void threshold(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+void threshold(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]){
   for (int x = 0; x < BMP_WIDTH; x++) {
     for (int y = 0; y < BMP_HEIGTH; y++) {      
-      for (int c = 0; c < BMP_CHANNELS; c++) {
-        if (input_image[x][y][0] <= 90) {
-          output_image[x][y][c] = 0;
-        } else {
-          output_image[x][y][c] = 255;
-        }
-      }
+      if (input_image[x][y] <= 90) {
+        output_image[x][y] = 0;
+      } else {
+        output_image[x][y] = 255;
+      }   
     }
   }
 }
 
-
-
-void erode(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+int erode(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]){
   
-  
-  
+  int change = 0;
+  // Se om dette kan fikses
   for (int x = 1; x < BMP_WIDTH-1; x++) {
     for (int y = 1; y < BMP_HEIGTH-1; y++) { 
-      
+
+      if (input_image[x][y] == 255) {
+  
+        if (input_image[x-1][y] == 0  || input_image[x][y-1] == 0 || input_image[x][y+1] == 0 || input_image[x+1][y] == 0) {
+          output_image[x][y] = 0;
+          change = 1;
+        } else {
+          output_image[x][y] = 255;
+        }
+      }
     }
   }
+  
+  input_image = output_image;
+  return change;
 }
 
 
@@ -106,12 +97,62 @@ int main(int argc, char** argv)
   read_bitmap(argv[1], input_image);
 
   //Run inversion
-  grayscale(input_image,output_image);
+  grayscale(input_image, temp_image);
 
-  threshold(output_image, output_image);
+  threshold(temp_image, temp_image);
 
+  erode(temp_image, temp_image2);
+ 
   //Save image to file
+
+
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      // se på om man kan optimere c ved at gøre det fra array til en integer
+      for (int c = 0; c < BMP_CHANNELS; c++){
+        output_image[x][y][c] = temp_image[x][y];
+      }
+    }
+  }
+
   write_bitmap(output_image, argv[2]);
+
+
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      // se på om man kan optimere c ved at gøre det fra array til en integer
+      for (int c = 0; c < BMP_CHANNELS; c++){
+        output_image[x][y][c] = temp_image2[x][y];
+      }
+    }
+  }
+  
+  write_bitmap(output_image, argv[2]+1);
+
+  int temp = 10;
+  while(temp) {
+    erode(temp_image2, temp_image);
+    temp--;
+  }
+
+
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      // se på om man kan optimere c ved at gøre det fra array til en integer
+      for (int c = 0; c < BMP_CHANNELS; c++){
+        output_image[x][y][c] = temp_image2[x][y];
+      }
+    }
+  }
+  
+  write_bitmap(output_image, argv[2]+2);
+
 
   printf("Done!\n");
   return 0;
